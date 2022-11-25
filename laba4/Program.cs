4f46ca45d960710c19f8a41806b12572746a15b2
viktor <topsonbog@gmail.com>
2022-11-26 01:53:36 +0700

    создано

---
+++ laba4/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1. Ввести A");
+Console.WriteLine("2. Ввести B");
+Console.WriteLine("3. Сложить");
+Console.WriteLine("4. Вычесть");
+Console.WriteLine("5. Умножить");
+Console.WriteLine("6. Поделить");
+int input = Convert.ToInt16(Console.ReadLine());
