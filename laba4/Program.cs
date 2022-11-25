4f8b083894f1d98073a97ca20a73116bae3f4a51
viktor <topsonbog@gmail.com>
2022-11-26 02:06:05 +0700

    добавлен выбор пункта

--- laba4/Program.cs
+++ laba4/Program.cs
@@ -6,3 +6,23 @@ Console.WriteLine("4. Вычесть");
 Console.WriteLine("5. Умножить");
 Console.WriteLine("6. Поделить");
 int input = Convert.ToInt16(Console.ReadLine());
+while(input < 1 || input > 6)
+{
+    Console.WriteLine("Неверный ввод. Попробуйте ещё раз.");
+    input = Convert.ToInt16(Console.ReadLine());
+}
+switch (input)
+{
+    case 1:
+        return;
+    case 2:
+        return;
+    case 3:
+        return;
+    case 4:
+        return;
+    case 5:
+        return;
+    case 6:
+        return;
+}
